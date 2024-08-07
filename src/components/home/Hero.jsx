45bd00a03172c1e9a7c2a77c38e0a85c1ce62d00
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-07 20:34:03 +0300

    Hero.jsx , react-icons insalled

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import styles from "./Hero.modules.scss";
+import { Button } from "../common/Button";
+import { CiSearch } from "react-icons/ci";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles.title}>
+        Find Home <span className={styles.primary}>Service/Repair</span>
+        <br />
+        Near You
+      </h1>
+      <p className={styles.subtitle}>
+        Explore Best Home Service & Repair near you
+      </p>
+      <div className={styles.searchContainer}>
+        <Button rounded>
+          <div>
+            <CiSearch fontSize={24} />
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
