639233ec9568943119198d034b82597fac5dccb4
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-08 18:00:28 +0300

    Home page created

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+// import Topbar from "../components/layout/Topbar";
+import Hero from "../components/home/Hero";
+import CategoryList from "../components/home/CategoryList";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <CategoryList />
+    </>
+  );
+}
