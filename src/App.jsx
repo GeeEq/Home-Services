2ca4088c264c9b8935c0b32398659b0b9d232575
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-08 15:35:48 +0300

    CategoryList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import Topbar from "./components/layout/Topbar";
 import Hero from "./components/home/Hero";
+import CategoryCard from "./components/home/CategoryCard";
 
 function App() {
   return (
     <>
       <Topbar />
       <Hero />
+      <CategoryCard />
     </>
   );
 }
--- src/components/home/CategoryCard.jsx
+++ src/components/home/CategoryCard.jsx
@@ -1,13 +1,21 @@
+import PropTypes from "prop-types";
 import styles from "./CategoryList.module.scss";
 
 export default function CategoryCard({ category }) {
-  const { name, icon } = category;
-  const Icon = icon;
+  const { name, icon: Icon, color } = category;
 
   return (
     <div className={styles.card}>
-      <Icon fontSize={48} color={category.color} />
+      <Icon fontSize={48} color={color} />
       <p className={styles.name}>{name}</p>
     </div>
   );
 }
+
+CategoryCard.propTypes = {
+  category: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    icon: PropTypes.elementType.isRequired,
+    color: PropTypes.string.isRequired,
+  }).isRequired,
+};
